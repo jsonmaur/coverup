24735f95bf6bb6db020d8a96af6c78ad31ddb067
Jason Maurer <j@maur.co>
2016-04-21 16:31:36 -0600

    use while loop over for loop

--- src/index.js
+++ src/index.js
@@ -4,31 +4,41 @@
  * @returns {string}
  */
 module.exports = function mask (value, options = {}) {
+  // ---------------------------------------------------------------------------
+  // option defaults
+  // ---------------------------------------------------------------------------
+
   options.keep = options.keep || 1
   options.direction = options.direction || 'left'
   options.character = options.character || '*'
 
-  if (typeof value !== 'string') {
-    throw new TypeError(`expected string, got ${typeof value}`)
+  // ---------------------------------------------------------------------------
+  // type casting and checks
+  // ---------------------------------------------------------------------------
+
+  if (!value) {
+    throw new Error('cannot mask an undefined value')
   }
 
-  if (typeof options.keep !== 'number') {
-    throw new TypeError(`expected number, got ${typeof options.keep}`)
-  }
+  value = String(value)
 
-  if (typeof options.character !== 'string') {
-    throw new TypeError(`expected string, got ${typeof options.character}`)
-  }
+  options.keep = Math.floor(parseInt(options.keep, 10))
+  options.character = String(options.character)
 
   if (!options.direction.match(/^(left|right)$/)) {
     throw new Error('direction must be either "left" or "right"')
   }
 
+  // ---------------------------------------------------------------------------
+  // mask
+  // ---------------------------------------------------------------------------
+
   let masked = options.direction === 'left'
     ? value.substring(0, options.keep) : ''
 
-  const padLength = value.length - options.keep
-  for (let i = 0; i < padLength; i++) {
+  let i = -1
+  const length = value.length - options.keep
+  while (++i < length) {
     masked += options.character
   }
 
--- test/index.test.js
+++ test/index.test.js
@@ -7,9 +7,6 @@ test((t) => {
   t.is(obfuscate('testing', { keep: 3 }), 'tes****')
   t.is(obfuscate('testing', { direction: 'right', keep: 5 }), '**sting')
 
-  t.throws(() => obfuscate(1), TypeError)
-  t.throws(() => obfuscate(), TypeError)
-  t.throws(() => obfuscate('testing', { keep: '3' }), TypeError)
-  t.throws(() => obfuscate('testing', { character: 1 }), TypeError)
+  t.throws(() => obfuscate(), Error)
   t.throws(() => obfuscate('testing', { direction: 'up' }), Error)
 })
